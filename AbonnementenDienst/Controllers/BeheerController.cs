b64783094f115e320d03cdaf8a909db1f1d57690
Daan Pape <daan@dptechnics.com>
2015-05-26 14:32:23 +0200

    started work on backoffice pages

--- AbonnementenDienst/Controllers/BeheerController.cs
+++ AbonnementenDienst/Controllers/BeheerController.cs
@@ -4,12 +4,23 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 
+using AbonnementenDienst.Models;
+
 namespace AbonnementenDienst.Controllers
 {
     public class BeheerController : Controller
     {
-        // GET: Beheer
+        private MagazineContext db = new MagazineContext();
+
+        // GET: index overzicht
         public ActionResult Index()
+        {
+            var magazines = db.Magazines.ToList();
+            return View(magazines);
+        }
+
+        // GET: Beheer
+        public ActionResult Create()
         {
             return View();
         }
